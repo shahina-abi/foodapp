df621823fc0cce04c4ae5f5d72b3230e71294667
shahina-abi <shahinakk300@gmail.com>
2025-02-19 14:53:59 +0400

    day23

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -19,12 +19,13 @@
 import React from "react";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/router";
-
+import { Toaster } from "react-hot-toast";
 const App = () => {
   return (
-    <React.StrictMode>
+    <>
       <RouterProvider router={router} />
-    </React.StrictMode>
+      <Toaster />
+    </>
   );
 };
 
